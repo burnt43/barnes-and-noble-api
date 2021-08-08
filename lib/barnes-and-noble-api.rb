dbcb42246d99a7006aee4f7b99b9ca9e54fd901a
burnt43 <jms.crsn@gmail.com>
2021-08-08 13:49:29 -0400

    add stubbing for testing

--- lib/barnes-and-noble-api.rb
+++ lib/barnes-and-noble-api.rb
@@ -14,6 +14,19 @@ module BanApi
       )
     end
 
+    # Public Stub Methods {{{
+    def stub_http_request!(url, params={}, return_value=nil)
+      @stubbed_http_requests ||= {}
+      @stubbed_http_requests[url] ||= {}
+      @stubbed_http_requests[url][params] = return_value
+    end
+
+    def clear_stubbed_http_request!
+      stubbed_http_requests.clear
+    end
+    # }}}
+
+    # Public API Methods {{{
     def search(term, number_of_results_per_page: 20, page: 1)
       make_request(
         "/s/#{CGI.escape(term)}",
@@ -23,14 +36,29 @@ module BanApi
         }
       )
     end
+    # }}}
 
     private
 
+    # Private Stub Methods {{{
+    def stubbed_http_requests
+      @stubbed_http_requests || {}
+    end
+
+    def stubbed_value(url, params={})
+      puts stubbed_http_requests.to_s
+      stubbed_http_requests.dig(url, params)
+    end
+    # }}}
+
     def make_request(url, params={}, type: :get)
-      # TODO: Add stub checking here, so we can stub in the test env.
-      @faraday_connection.send(type, url) do |req|
-        params.each do |param_name, param_value|
-          req.params[param_name] = param_value
+      if (my_stubbed_value = stubbed_value(url, params))
+        my_stubbed_value
+      else
+        @faraday_connection.send(type, url) do |req|
+          params.each do |param_name, param_value|
+            req.params[param_name] = param_value
+          end
         end
       end
     end
--- test/api_test.rb
+++ test/api_test.rb
@@ -5,6 +5,13 @@ module BanApi
     class ApiTest < BanApi::Testing::Test
       def test_foobar
         api = BanApi::Api.new
+        api.stub_http_request!(
+          '/s/battle+angel+alita',
+          {'Nrpp' => 20, 'page' => 1},
+          'test'
+          # IO.read('./test/assets/stubs/http_responses/battle_angel_alita_search.html')
+        )
+
         puts api.search('battle angel alita')
       end
     end
